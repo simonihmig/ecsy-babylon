5d7cd2f3f29412bc4da4abc4d850d922677fcfcc
simonihmig <simon.ihmig@kaliber5.de>
2022-01-18 16:22:38 +0100

    Add missing color properties to lights

--- src/components/light/_light.ts
+++ src/components/light/_light.ts
@@ -1,9 +1,15 @@
 import { Component, ComponentSchema, Types } from 'ecsy';
+import { Color3 } from '@babylonjs/core/Maths/math.color';
+import { BabylonTypes } from '../../-private/ecsy-types';
 
 export default abstract class Light<C> extends Component<C> {
   intensity!: number;
+  diffuse!: Color3;
+  specular!: Color3;
 }
 
 export const schema: ComponentSchema = {
   intensity: { type: Types.Number, default: 1 },
+  diffuse: { type: BabylonTypes.Color3, default: Color3.White() },
+  specular: { type: BabylonTypes.Color3, default: Color3.White() },
 };
--- src/components/light/hemispheric.ts
+++ src/components/light/hemispheric.ts
@@ -3,12 +3,15 @@ import { schema as baseSchema } from './_shadow';
 import { ComponentSchema } from 'ecsy';
 import { BabylonTypes } from '../../-private/ecsy-types';
 import Light from './_light';
+import { Color3 } from '@babylonjs/core/Maths/math.color';
 
 export default class HemisphericLight extends Light<HemisphericLight> {
   direction!: Vector3;
+  groundColor!: Color3;
 
   static schema: ComponentSchema = {
     ...baseSchema,
     direction: { type: BabylonTypes.Vector3, default: new Vector3(0, -1, 0) },
+    groundColor: { type: BabylonTypes.Color3, default: Color3.Black() },
   };
 }
